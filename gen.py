83a932e778cb470b35cdb69fd788d2b761d798d2
Cliff Xuan <cliff.xuan@gmail.com>
2021-09-07 22:46:16 +0100

    gen

---
+++ gen.py
@@ -0,0 +1,82 @@
+import json
+import jinja2
+from pathlib import Path
+import re
+
+PWD = Path(__file__).absolute().parent
+
+env = jinja2.Environment()
+
+
+CAP_RE = re.compile("(.)([A-Z])")
+LEADING_UNDERSCORE = re.compile("^(_*)")
+
+
+def to_pascal_case(name):
+    """
+    Turn a snake_case name into PascalCase.
+    """
+    return name.title().replace("_", "")
+
+
+def from_pascal_case(name):
+    """
+    Turn a PascalCase name into snake_case.
+    """
+    return CAP_RE.sub(r"\1_\2", name).lower()
+
+
+def to_camel_case(name):
+    """
+    Turn a snake_case name into camelCase.
+    """
+    mat = LEADING_UNDERSCORE.match(name)
+    leading_underscores = mat.group()
+
+    output = name[len(leading_underscores) :].title().replace("_", "")
+    return output
+
+
+def to_kebab_case(name):
+    """
+    Turn a snake_case name into kebab-case
+    """
+    return name.replace("_", "-")
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
+
+def process(policy_id: str, token: str, nft: dict) -> dict:
+    ipfs = nft["image"].split("://")[1]
+    nft["policy_id"] = policy_id
+    nft["token"] = token
+    nft["url"] = f"./nft/{to_kebab_case(from_pascal_case(token))}.png"
+    nft["ipfs"] = ipfs
+    nft["pool_pm"] = f"https://pool.pm/{policy_id}.{token}"
+    # nft["ipfs_url"] = f"https://ipfs.io/ipfs/{ipfs}"
+    nft["ipfs_url"] = f"https://cloudflare-ipfs.com/ipfs/{ipfs}"
+    return nft
+
+
+def main():
+    with open(PWD / "templates" / "nft.jinja") as t:
+        template = env.from_string(t.read())
+
+    metadata = json.load(open(PWD / "nft" / "block-dragon.json"))
+    policy_id = list(list(metadata.values())[0].keys())[0]
+    nfts = [
+        process(policy_id, token, nft)
+        for token, nft in list(list(metadata.values())[0].values())[0].items()
+    ]
+    groups = [group for group in chunks(nfts, 3)]
+    result = template.render(groups=groups)
+    with open(PWD / "index.html", "w") as f:
+        f.write(result)
+
+
+if __name__ == "__main__":
+    main()
